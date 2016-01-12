f2935b7ed1ca61cab3db1b58c3e5ad7994a505d3
Guest User <student@epicodus.com>
2016-01-12 07:42:12 -0800

    updated from home

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+  //-$("div").click(function(){
+  //  $(".clickIt").toggle();
+  //})
+
+  $("div").click(function() {
+      $("#JavaScript").toggle();
+
+  });
+
+  $("p").click(function() {
+      $("#Modulo").toggle();
+      $("#ModuloShowing").toggle();
+    });
+      $("#Operators").toggle();
+      $("#Nan").toggle();
+      $("#Variable Naming Conventions").toggle();
+      $("#+=").toggle();
+      $("#Function").toggle();
+      $("#Method").toggle();
+      $("#Argument to Method").toggle();
+      $("#String").toggle();
+      $("#Chaining Methods").toggle();
+      $("#Escape").toggle();
+      $("#Alert").toggle();
+      $("#Commenting Code").toggle();
+      $("#Boolean").toggle();
+      $("#Undefined").toggle();
+      $("#Parameter").togle();
+      $("#Return").toggle();
+
+    });
+
+  });
